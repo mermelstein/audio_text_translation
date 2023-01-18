aa4227de65cab27cc9a5cabfe999e38baa8b9594
Dani Mermelstein <dani.mermelstein@gmail.com>
2023-01-18 17:31:16 -0500

    allowing for multiple language selection

--- main.py
+++ main.py
@@ -15,23 +15,23 @@ dg_client = Deepgram(os.getenv("DEEPGRAM_API_KEY"))
 
 templates = Jinja2Templates(directory="templates")
 
-async def process_audio(fast_socket: WebSocket):
+async def process_audio(fast_socket: WebSocket, language_var):
     async def get_transcript(data: Dict) -> None:
-        if 'channel' in data:
+        if 'channel' in data and data['channel']['alternatives'][0]['confidence'] >= 0.00:
             transcript = data['channel']['alternatives'][0]['transcript']
-            translation = translate(transcript)
+            # translation = 
 
             if transcript:
                 await fast_socket.send_text(transcript)
-                await fast_socket.send_text(translation)
+                # await fast_socket.send_text(translate(transcript, language_var[0]))
 
-    deepgram_socket = await connect_to_deepgram(get_transcript)
+    deepgram_socket = await connect_to_deepgram(get_transcript, language_var)
 
     return deepgram_socket
 
-async def connect_to_deepgram(transcript_received_handler: Callable[[Dict], None]):
+async def connect_to_deepgram(transcript_received_handler: Callable[[Dict], None], language_var):
     try:
-        socket = await dg_client.transcription.live({'punctuate': True, 'interim_results': False, 'language': 'en-US'})
+        socket = await dg_client.transcription.live({'punctuate': True, 'interim_results': False, 'language': language_var[1]})
         socket.registerHandler(socket.event.CLOSE, lambda c: print(f'Connection closed with code {c}.'))
         socket.registerHandler(socket.event.TRANSCRIPT_RECEIVED, transcript_received_handler)
         
@@ -48,7 +48,11 @@ async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
 
     try:
-        deepgram_socket = await process_audio(websocket) 
+        # language_var = ['en','en-US']
+        language_var = ['es','es-419']
+        # language_var = ['zh','zh-CN']
+
+        deepgram_socket = await process_audio(websocket, language_var) 
         
         while True:
             data = await websocket.receive_bytes()
--- translate_text.py
+++ translate_text.py
@@ -4,9 +4,18 @@ import openai
 # Load your API key from an environment variable or secret management service
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-prompt_text = "translate the following text into Mandarin Chinese: "
+english_prompt_text = "translate the following American English text into Mandarin Chinese: "
+spanish_prompt_text = "translate the following Spanish text into American English: "
+chinese_prompt_text = "translate the following Mandarin Chinese text into English: "
 
-def translate(text):
+def translate(text, language_var):
+    if language_var == 'en':
+        prompt_text = english_prompt_text
+    elif language_var == 'es':
+        prompt_text = spanish_prompt_text
+    elif language_var == 'zh':
+        prompt_text = chinese_prompt_text
+
     response = openai.Completion.create(
         model="text-davinci-003", prompt=prompt_text + text, temperature=0, max_tokens=200
     )
